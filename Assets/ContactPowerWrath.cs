a07e78452c6e447907056703c8e5edb21d04326e
SeindElBardo <seindelbardo@gmail.com>
2020-09-22 22:57:35 +0200

    Please, don't explode

---
+++ Assets/ContactPowerWrath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactPowerWrath : MonoBehaviour
+{
+    //[SerializeField] private Collider2D _collider2D;
+    [SerializeField] private int _damage;
+    
+    private void Activate(GameObject victim)
+    {
+        //victim.TakeDamage();
+        print("jaja");
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player")){
+            Activate(collision.gameObject);
+        }
+    }
+}
+
